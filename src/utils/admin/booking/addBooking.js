3f96b8540c8e0e84d77b1f8f8f8170abc54e1434
florine <florinepltg64@gmail>
2021-03-31 18:55:48 +0200

    patch

--- src/utils/admin/booking/addBooking.js
+++ src/utils/admin/booking/addBooking.js
@@ -2,14 +2,14 @@ import axios from "axios";
 import {getCookie} from "../../cookie/cookie";
 
 
-export function addBooking(nbTableSpace1, nbTableSpace2, nbTableSpace3, nbM2Space1, nbM2Space2, nbM2Space3, totalPrice, animatorNeeded, festivalId, exhibitorId) {
+export function addBooking(nbTableSpace1, nbTableSpace2, nbTableSpace3, nbM2Space1, nbM2Space2, nbM2Space3, animatorNeeded, festivalId, exhibitorId) {
     let burl = process.env.VUE_APP_API;
     console.log(burl);
     return axios({
         method: 'post',
         url: burl + '/api/booking/add/',
         data : {
-            nbTableSpace1, nbTableSpace2, nbTableSpace3, nbM2Space1, nbM2Space2, nbM2Space3, totalPrice, animatorNeeded, festivalId, exhibitorId
+            nbTableSpace1, nbTableSpace2, nbTableSpace3, nbM2Space1, nbM2Space2, nbM2Space3, animatorNeeded, festivalId, exhibitorId
         },
         headers : {
             'Content-Type': 'application/json',
